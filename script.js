20c4ac143a54d04dfa67b9a14bd558d1f9b3e463
Unknown <rolandojpena88@gmail.com>
2018-06-28 08:38:25 -0500

    upload of background image and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+body {
+    background: \images\bg.jpg no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+	color:#fff;
+  	background-color:#333;
+}
